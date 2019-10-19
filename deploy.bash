c1214a05cb165335a2adf22739fcda02d15f3985
Julius Ting <jul_ting@yahoo.com>
2019-10-19 11:37:25 +0800

    init:

---
+++ deploy.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Deploying to Google Cloud with Project ID"
+config="$(gcloud config configurations list | grep artofthemoon | awk '{print $1}')"
+gcloud config configurations list | grep artofthemoon
+gcloud config configurations activate ${config}
+gcloud app deploy --project=artofthemoon --quiet
