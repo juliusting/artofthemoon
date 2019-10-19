6b08988d219b53e8525995e4da5800748488a1db
WtH <wth193@gmail.com>
2019-10-20 04:56:03 +0800

    d

--- src/components/Apollo11Day4.vue
+++ src/components/Apollo11Day4.vue
@@ -1,17 +1,32 @@
 <template>
-	<section>
+	<section  v-inview:enter="onEnter" v-inview:leave="onLeave">
 	</section>
 </template>
 
 <script>
 export default {
 	name: "apollo11Day4",
-	components: {},
+	inject: ['$util'],
 	data () {
 		return {
 		}
 	},
 	methods: {
+		callbackEagle (resolve, reject, transcript) {
+			this.$util.getSpeechDestroy()
+			resolve(true)
+		},
+		async onEnter () {
+			let transcript = await this.$util.getSpeech(this.callbackEagle).catch(error => console.warn('ops =)'))
+
+			if (transcript) {
+				this.$util.playAudio('eagle')
+			}
+		},
+		onLeave () {
+			this.$util.stopAudio('eagle')
+			this.$util.getSpeechDestroy()
+		}
 	},
 	created() {
 
@@ -22,7 +37,9 @@ export default {
 <style lang="scss" scoped>
 section {
 	background-color: black;
-	background-image: url('@/../../assets/manonmoon.jpg');
-	position: absolute;
+	background-image: url('@/../../assets/images/eagle.gif');
+	// background-image: url('@/../../assets/manonmoon.jpg');
+	background-position: center;
+	background-size: cover;
 }
 </style>
