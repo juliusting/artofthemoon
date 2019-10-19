29c1789ab8670e56dae909565d1b583f9331a1f0
Julius Ting <jul_ting@yahoo.com>
2019-10-19 15:59:01 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class>hello</h1>
+    <canvas id="canvasScreen" width="100" height="100"></canvas>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+var canvas = null;
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
+  },
+  methods: {
+    draw: function() {
+
+    }
+  },
+  created() {
+
   }
-}
+};
 </script>
+
+<style scoped>
+#canvasScreen {
+  position: fixed;
+  top: 0;
+}
+</style>
