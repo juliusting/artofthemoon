035817d0fb63d8b851fdf2b0682c7f1fe5c4eec5
lololipop41 <56724086+lololipop41@users.noreply.github.com>
2019-10-20 00:37:17 +0800

    added few scenes

--- src/components/Apollo11Day1.vue
+++ src/components/Apollo11Day1.vue
@@ -1,5 +1,13 @@
 <template>
 	<section>
+		<div class="JKR">
+			<div style="margin-left: auto; width: 10em">
+				<img src="@/assets/presidentspeech.png" alt="president" class="speech">
+			</div>
+		</div>
+		<div class="s">
+			<p>"But why, some say, the moon?"</p>
+		</div>
 	</section>
 </template>
 
@@ -21,6 +29,27 @@ export default {
 
 <style lang="scss" scoped>
 section {
-	background-color: green;
+	padding: 0.01em 16px;
+	background-color: black;
+}
+
+p {
+  margin: 0;
+}
+
+.s {
+  color: white;
+  font-size: 5em;
+  margin: -7em 0em 0em 1em;
+  text-align-last: left;
+}
+
+.speech {
+	width: 23em;
+	margin: 22em 0em 0em -12em;
+}
+
+.JKR {
+	display: flex;
 }
 </style>
--- src/components/Apollo11Landing.vue
+++ src/components/Apollo11Landing.vue
@@ -1,92 +1,37 @@
 <template>
 	<section>
-        <canvas ref="canvas" width="windowWidth" height="windowHeight"></canvas>
         <div class="content">
-        <div class="container1">
-            <div class="video">
-            <video autoplay="" muted="" loop="" id="mVideo" style="display:block;margin:auto" width="100%" height="100%">
-                <source src="@/assets/moon.mp4" type="video/mp4">
-            </video>
-            <div class="video-desc">
-                <div class="title">
-                <p class="BigTitle">The Art of Moon!</p>
-                <div class="desc">
-                    <p>Based on real mission </p>
-                    <p>of </p>
-                    <p>Apollo 11 in 1969</p>
+          <div class="container1">
+              <div class="video">
+                <video autoplay="" muted="" loop="" id="mVideo" style="display:block;margin:auto" width="90%" height="90%">
+                    <source src="@/assets/moon.mp4" type="video/mp4">
+                </video>
+                <div class="video-desc">
+                    <div class="title">
+                      <p class="BigTitle">Apollo11</p>
+                      <div class="desc">
+                          <p>Based on real mission </p>
+                          <p>in </p>
+                          <p>1969</p>
+                      </div>
+                    </div>
                 </div>
-                </div>
-            </div>
-            </div>
-        </div>
-        <div class="c1">
-            <p class="earth">This is the Earth</p>
-        </div>
-        <div>
-            <p class="right-side">hello!</p>
-        <p class="right-side">hello!</p>
-        <p class="right-side">hello!</p>
-        <p class="right-side">hello!</p>
-        <p class="right-side">hello!</p>
-        <p class="right-side">hello!</p>
-        <p class="right-side">hello!</p>
-        <p class="right-side">hello!</p>
-        <p class="right-side">hello!</p>
-        <p class="right-side">hello!</p>
-        <p class="right-side">hello!</p>
-        <p class="right-side">hello!</p>
-        <p class="right-side">hello!</p>
-        <p class="right-side">hello!</p>
-        <p class="right-side">hello!</p>
-        <p class="right-side">hello!</p>
-        <p class="right-side">hello!</p>
-        </div>
+              </div>
+          </div>
+          <div class="arrow">
+            <img src="@/assets/down-arrow.svg" alt="down-arrow" class="darrow">
+          </div>
         </div>
 	</section>
 </template>
 
 <script>
-const bg = new Image();
-bg.src = require("../assets/3.4.earth-min.png");
-
 export default {
   name: "apollo11Landing",
   data () {
-      return {
-           windowWidth: window.innerWidth,
-            windowHeight: window.innerHeight
-      }
   },
   components: {},
   methods: {
-    draw: function() {
-      this.windowWidth = window.innerWidth;
-      this.windowHeight = window.innerHeight;
-      this.canvas.width = window.innerWidth;
-      this.canvas.height = window.innerHeight;
-      this.ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-      this.ctx.drawImage(
-        bg,
-        window.innerWidth / 2 - 250,
-        window.innerHeight / 2 - 250,
-        500,
-        500
-      );
-
-      requestAnimationFrame(this.draw);
-    }
-  },
-  mounted() {
-    this.canvas = this.$refs.canvas;
-    this.ctx = this.$refs.canvas.getContext("2d");
-    this.canvas.width = window.innerWidth;
-    this.canvas.height = window.innerHeight;
-
-    this.ctx.beginPath();
-    this.ctx.strokeStyle = "black";
-    this.ctx.rect(0, 0, 200, 200);
-    this.ctx.stroke();
-    this.draw();
   }
 };
 
@@ -98,9 +43,11 @@ section {
 }
 
 canvas {
-  position: fixed;
+  position: absolute;
   top: 0;
   left: 0;
+  bottom: 0;
+  right: 0;
   background: black;
   z-index: -3;
 }
@@ -114,17 +61,11 @@ p {
 .container1 {
   padding: 0.01em 16px;
 }
-.c1 {
-  font-size: 10px;
-  margin: 17em 52em 0em 0em;
-  background-image: url(/assets/presidentspeech.png);
-  z-index: 3;
-}
 
 .BigTitle {
   font-size: 8em;
   font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-  margin: 10% 10% 0% 10%;
+  margin: 36% 0% 0% 0%;
 }
 
 .desc {
@@ -140,7 +81,15 @@ p {
 .video-desc {
   position: absolute;
   top: 50%;
-  left: 16%;
+  left: 31%;
   margin: -20em 0em 0em 0em;
+}
+
+.darrow {
+  width: 40px;
+  position: relative;
+  animation: arrowmove 1.5s infinite;
+  animation-timing-function: ease;
+  margin: -3em 0em 1em 0em;
 }
 </style>
