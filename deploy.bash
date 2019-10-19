7e9dc68c907d578d3690c91934dcdb33287a3aa0
Julius Ting <jul_ting@yahoo.com>
2019-10-19 14:53:28 +0800

    ngeh

--- deploy.bash
+++ deploy.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+npm run build
 echo "Deploying to Google Cloud with Project ID"
 config="$(gcloud config configurations list | grep artofthemoon | awk '{print $1}')"
 gcloud config configurations list | grep artofthemoon
