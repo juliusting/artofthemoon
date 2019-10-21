1c6ca93264466162f4f01063daa96bc45d7d0e52
WtH <wth193@gmail.com>
2019-10-21 10:24:56 +0800

    stripped about page

--- src/router/index.js
+++ src/router/index.js
@@ -12,16 +12,6 @@ const routes = [
 		redirect: '/apollo11'
 	},
 	{
-		path: '/about',
-		name: 'about',
-		// route level code-splitting
-		// this generates a separate chunk (about.[hash].js) for this route
-		// which is lazy-loaded when the route is visited.
-		component: function () {
-		return import(/* webpackChunkName: "about" */ '../views/About.vue')
-		}
-	},
-	{
 		path: '/apollo11',
 		name: 'apollo11',
 		component: _ => import('../views/Apollo11.vue')
